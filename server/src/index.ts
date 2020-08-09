09b66f5a1c5fb4ffb75129e21346fd386ba582ae
Andrew <andrewdymov@gmail.com>
2020-08-09 19:33:15 +0300

    Create proxy to access VK api from Ukraine

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,9 +4,11 @@
 
 import fs from 'fs'
 import pug from 'pug'
+import got from 'got'
 import path from 'path'
 import dotenv from 'dotenv'
 import express from 'express'
+import proxy from 'express-http-proxy'
 
 dotenv.config()
 
@@ -26,55 +28,62 @@ import { init as initWebSocket } from './websocket'
 type PugsKeys = 'anomaly' | 'dashboard'
 
 export type PugTemplates = {
-    [key in PugsKeys]?: pug.compileTemplate
+  [key in PugsKeys]?: pug.compileTemplate
 }
 
 const pugs: PugTemplates = {}
 const templatesPath = path.resolve(__dirname, '../templates')
 
 fs.readdirSync(templatesPath).forEach((file: string): void => {
-    if (file.endsWith('.pug')) {
-        try {
-            const filepath = path.resolve(templatesPath, file)
-            pugs[file.split('.pug')[0]] = pug.compile(fs.readFileSync(filepath, { encoding: 'utf-8' }), {
-                filename: filepath
-            })
-        } catch (e) {
-            console.error(e)
-        }
+  if (file.endsWith('.pug')) {
+    try {
+      const filepath = path.resolve(templatesPath, file)
+      pugs[file.split('.pug')[0]] = pug.compile(fs.readFileSync(filepath, { encoding: 'utf-8' }), {
+        filename: filepath
+      })
+    } catch (e) {
+      console.error(e)
     }
+  }
 })
 
 const app = express()
 
+app.use('/proxy', proxy('api.vk.com'))
 app.use('/images', express.static('images'))
 app.use(express.static('public'))
 
 app.disable('x-powered-by')
 
 app.get('/render', async (req, res) => {
-    try {
-        const resp = await anomaly(pugs.anomaly, req)
-        res.status(resp.code).set(resp.headers).send(resp.body)
-    } catch (e) {
-        console.error(e)
-        res.send(500)
-    }
+  try {
+    const resp = await anomaly(pugs.anomaly, req)
+    res.status(resp.code).set(resp.headers).send(resp.body)
+  } catch (e) {
+    console.error(e)
+    res.send(500)
+  }
 })
 
 app.get('/', async (req, res) => {
-    try {
-        const resp = await dashboard(pugs.dashboard)
-        res.send(resp)
-    } catch (e) {
-        console.error(e)
-        res.send(500)
-    }
+  try {
+    const resp = await dashboard(pugs.dashboard)
+    res.send(resp)
+  } catch (e) {
+    console.error(e)
+    res.send(500)
+  }
 })
 
 app.listen(parseInt(process.env.EXPRESS_PORT), 'localhost', () => {
-    console.log('Express server is up and running...')
-    initWebSocket()
-    console.log('Websocket server is up and running...')
-    initProcessor()
+  console.log('Express server is up and running...')
+  initWebSocket()
+  console.log('Websocket server is up and running...')
+  initProcessor()
+
+  got('https://api.vk.com/method/users.get?user_id=1').catch(e => {
+    if ((e.message as string).includes('connect ECONNREFUSED')) {
+      process.env.USE_PROXY = 'true'
+    }
+  })
 })
