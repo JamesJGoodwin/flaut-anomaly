875050592873987bd33e560118f47dd7eb2b018b
Andrew <andrewdymov@gmail.com>
2020-09-09 22:12:33 +0300

    Add autorestart and time prefix in logs for PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-    apps: [
-        {
-            name: 'Anomaly',
-            script: './server/build/index.js',
-            watch: ['server/build', 'public/bundle'],
-            env: {
-                NODE_ENV: 'production'
-            },
-            restart_delay: 172_800_000
-        }
-    ]
+  apps: [
+    {
+      name: 'Anomaly',
+      script: './server/build/index.js',
+      watch: ['server/build', 'public/bundle'],
+      env: {
+        NODE_ENV: 'production'
+      },
+      cron_restart: '0/3 0 0/1 ? * 2/3 *', // restart every third day starting from monday
+      time: true
+    }
+  ]
 }
