584bee04212601023a7850acd0b1c910fee89a02
Andrew <andrewdymov@gmail.com>
2020-09-09 22:45:15 +0300

    Remove cron restart (faulty strategy)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,6 @@ module.exports = {
       env: {
         NODE_ENV: 'production'
       },
-      cron_restart: '0/3 0 0/1 ? * 2/3 *', // restart every third day starting from monday
       time: true
     }
   ]
